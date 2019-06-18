c7b0d052dab90703452d57225c586bbac0c345c2
Jacob Clark <jacob.clark@bbc.co.uk>
2019-06-18 22:26:50 +0100

    Process user events in batch

--- serverless/index.js
+++ serverless/index.js
@@ -2,15 +2,23 @@ const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = event => {
-    event.Records.forEach(record => {
-        let recordToWrite = JSON.parse(new Buffer(record.body, 'base64').toString('ascii')).event;
+    const decodedEvents = event
+        .Records
+        .map(record => JSON.parse(new Buffer(record.body, 'base64').toString('ascii')).event);
+
+    const uniqueUserIdsFromBatch = [...new Set(decodedEvents.map(event => event.userId))];
+    
+    uniqueUserIdsFromBatch.map(userId => {
+        const uniqueUsersEvents = decodedEvents
+            .filter(event => event.userId === userId)
+            .reduce((previous, current) => [...current.events, ...previous], []);
 
         dynamodb.update({
             TableName: 'OdoStatTable',
-            Key: { userId: recordToWrite.userId },
+            Key: { userId: userId },
             UpdateExpression: 'set events = list_append(if_not_exists(events, :empty_list), :value)',
             ExpressionAttributeValues: {
-                ':value': recordToWrite.events,
+                ':value': uniqueUsersEvents,
                 ':empty_list': []
             }
         }, (err, data) => err ? console.error('ERROR: ' + err) : console.info('OK: ' + JSON.stringify(data, null, '  ')));
