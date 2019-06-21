b65b2248163d5a0b7be1e75c8cc323cd0b5063f8
Jacob Clark <jacob.clark@bbc.co.uk>
2019-06-21 18:01:40 +0100

    Write calculations to DyanmoDB

--- serverless/eventCalculation.js
+++ serverless/eventCalculation.js
@@ -2,18 +2,68 @@ const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = event => {
-    const events = {};
+    const stats = {
+        "projects": {},
+    };
     
+    let userId = "";
+
     event
         .Records
-        .forEach(record => record
-            .dynamodb
-            .NewImage
-            .events
-            .L
-            .forEach(event => console.log(event.M)));
+        .forEach(record => {
+            const newImage = record
+                .dynamodb
+                .NewImage;
+                
+            if (!newImage) return;
+
+            userId = newImage.userId.S
+
+            // Need to add languages summary within projects {}
+            return newImage.events.L.forEach(event => {
+                let project = stats["projects"][event.M.project.S];
+
+                if (project){
+                    const file = project
+                        .files
+                        .find(file => {
+                            return event.M.file.S === file.fileName
+                        });
+
+                    if (file) {
+                        file.elapsed = new Number(file.elapsed) + new Number(event.M.elapsed.N);
+                    } else {
+                        project.files.push({
+                            fileName: event.M.file.S,
+                            language: event.M.language.S,
+                            elapsed: event.M.elapsed.N
+                        });
+                    }
+                }else{
+                    stats["projects"][event.M.project.S] = {
+                        files: [
+                            {
+                                fileName: event.M.file.S,
+                                language: event.M.language.S,
+                                elapsed: event.M.elapsed.N
+                            }
+                        ]
+                    }
+                };
+            });
+        });
+
+    // We should convert the date to an epoch, have it as the sort key and put a new record per date
+    dynamodb.put({
+        Item: {
+            "userId": userId,
+            "stats": stats
+        },
+        TableName: "OdoCalculationTable"
+    }, (err, data) => err ? console.error('ERROR: ' + err) : console.info('OK: ' + JSON.stringify(data, null, '  ')));
 
     return {
         statusCode: 200
     }
 };
+
